d7abdc5b9bdbf8c601ce26fb874bfa16806f4bbc
Tiago Almeida <soares343vio@hotmail.com>
2021-08-25 21:57:38 +0100

    Delivers invalid data error on non 200 HTTP request

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,8 +19,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { _ in
-			completion(.failure(Error.connectivity))
+		client.get(from: url) { result in
+			switch result {
+				case .success:
+					completion(.failure(Error.invalidData))
+				default:
+					completion(.failure(Error.connectivity))
+			}
 		}
 	}
 }
